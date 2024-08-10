7306e4be0aa890241a22a7b6b9bcb257ffea0143
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-08-10 13:02:13 +0100

    ci(monorepo): Resolved issue with setup workflow actions

--- tools/scripts/unlock-monorepo.sh
+++ tools/scripts/unlock-monorepo.sh
@@ -2,12 +2,13 @@
 
 if [ "$SHOULD_UNLOCK_GIT_CRYPT" = "1" ]; then
     echo "Added git-crypt and git-lfs"
-    apk --update add git-crypt git-lfs
+
+    curl -L https://github.com/AGWA/git-crypt/releases/download/0.7.0/git-crypt-0.7.0-linux-x86_64 \
+        -o ./git-crypt
 
     echo "Unlocking monorepo..."
     git-crypt unlock
 
-  fi
 else
   echo "Not unlocking - SHOULD_UNLOCK_GIT_CRYPT is not set"
 fi
