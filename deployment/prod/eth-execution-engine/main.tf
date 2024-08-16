2342403f10a98ff049732e87dcd53b4470d8f1ae
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-08-16 10:18:19 +0100

    fix(eth-execution-telegram-bot): Added missing `main.tf` file

---
+++ deployment/prod/eth-execution-engine/main.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.62.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
