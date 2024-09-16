e97f40e4624e418b3e1e1bbf9fdf48ba682126f4
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-09-16 07:48:49 +0100

    fix(reaper-telemetry): Resolve issue with invalid use of sentry module

--- crates/telemetry/src/sentry.rs
+++ crates/telemetry/src/sentry.rs
@@ -1,7 +1,9 @@
+use sentry::{init, ClientOptions, release_name};
+
 /// Initialize Sentry with the given DSN.
 pub fn init_sentry(url: &str) {
-    let _guard = sentry::init((url, sentry::ClientOptions {
-        release: sentry::release_name!(),
+    let _guard = init((url, ClientOptions {
+        release: release_name!(),
         ..Default::default()
       }));
 }
